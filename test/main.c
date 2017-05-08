369d03941796f02594714c22f4078af281cef0cc
kabsly <kabsly@users.noreply.github.com>
2017-05-08 18:21:26 -0500

    Updated test main

--- test/main.c
+++ test/main.c
@@ -4,14 +4,14 @@
 #include <pak.h>
 
 #include "pak_list_test.h"
-#include "pak_vec_test.h"
+#include "pak_arr_test.h"
 #include "pak_alg_test.h"
 
 int main()
 {
     pak_test_init();
 
-    pak_test_begin(pak_vec_test);
+    pak_test_begin(pak_arr_test);
     pak_test_begin(pak_alg_test);
     pak_test_begin(pak_list_test);
 
